b62321a511aab4026d598099f285d5f85dabee60
pouple <antonin.verdier@laposte.net>
2018-11-27 17:26:58 +0100

    Replacement of np.dot function by unoptimized custom function for multi-core processing. Separation of plotting function

--- Bump.py
+++ Bump.py
@@ -2,16 +2,23 @@
 import numpy as np
 from scipy.linalg import circulant 
 from tqdm import tqdm
+import time
+import pickle
 
+import argparse
 
 import matplotlib.pyplot as plt
 plt.ion()
 
-def noise(sigE, sigI):
-	noise_E = sigE * np.random.randn(nb_neurons, 1)
-	noise_I = sigI * np.random.randn(nb_neurons, 1)
+def generate_stimulus(stimon, stimoff, stim, delayend):
+	v = np.exp(kappa*np.cos(theta))
+	stimulus = np.array(stim * v).reshape(nb_neurons, 1)
+	stimon = np.floor(stimon/dt)
+	stimoff = np.floor(stimoff/dt)
+	delayend = np.floor(delayend/dt)
+	delaywin = np.floor(100/dt)
 
-	return noise_E, noise_I
+	return stimulus, stimon, stimoff, delayend, delaywin
 
 def f(x):
 	tmp = []
@@ -28,16 +35,20 @@ def f(x):
 def decode(r, th):
 	return np.arctan2(sum(np.multiply(r, np.sin(th))), sum(np.multiply(r, np.cos(th))))
 
-# Dynamic plotting
-def plotting(data, theta):
-	for step in data:
-		plt.xlim((-180, 180))
-		plt.ylim((-5, 20))
-		plt.title('Iteration {}'.format(step))
-		plt.plot(theta*180/np.pi, data[step])
-		plt.pause(0.001)
-		plt.show()
-		plt.clf()
+
+def noise(sigE, sigI):
+	noise_E = sigE * np.random.randn(nb_neurons, 1)
+	noise_I = sigI * np.random.randn(nb_neurons, 1)
+
+	return noise_E, noise_I
+
+def matrix_product(X, Y):
+	Z = np.zeros([512, 1])
+	for i in range(X.shape[0]):
+		for j in range(Y.shape[1]):
+			Z[i, j] = np.sum(X[:1]*np.transpose(Y[:, :1]))
+
+	return Z
 
 # Variable definition
 nb_neurons = 512
@@ -53,7 +64,7 @@ tauE = 20
 tauI = 10
 
 # Connection matrices
-kappa = 1.5
+kappa = 5
 GEE = 6
 GIE = 4
 GEI = 3.4
@@ -62,18 +73,14 @@ GII = 0.85
 I0E = 0.2
 I0I = 0.5
 
+stim = 200
+
 sigE = 1
 sigI = 3
 
-stimon = 1000
-stimoff = 1500
-stim = 200
-delayend = 3500
-
 theta = np.array([i/nb_neurons*2*np.pi for i in range(0, nb_neurons)]) # Need to find a more elegant way
 v = np.exp(kappa*np.cos(theta))
-v = v/sum(v)
-WE = circulant(v)
+WE = circulant(v/sum(v))
 
 # Define iterations when stimulus is applied
 
@@ -86,25 +93,23 @@ rI = np.zeros([nb_neurons, 1])
 theta = theta - np.pi
 v = np.exp(kappa*np.cos(theta))
 v = v/sum(v)
-stimulus = np.array(stim * v).reshape(512, 1)
-stimon = np.floor(stimon/dt)
-stimoff = np.floor(stimoff/dt)
-delayend = np.floor(delayend/dt)
-delaywin = np.floor(100/dt)
 
+stimulus, stimon, stimoff, delayend, delaywin = generate_stimulus(1000, 1500, 200, 3500)
 
 data = {}
 for i in tqdm(range(1, nb_steps), ascii=True):
 	# Additive noise
 	noise_E, noise_I = noise(sigE, sigI)
 
-	IE = GEE*np.matmul(WE, rE) + (I0E - GIE*np.mean(rI))*np.ones([nb_neurons, 1])
+	Z = matrix_product(WE, rE)
+	
+	IE = GEE*Z + (I0E - GIE*np.mean(rI))*np.ones([nb_neurons, 1])
 	II = (GEI*np.mean(rE) - GII*np.mean(rI) + I0I)*np.ones([nb_neurons, 1])
 
-	if i > stimon and i < stimoff:
+	if stimon < i < stimoff:
 		IE = IE + stimulus
 
-	if i > delayend and i < delayend + (stimoff - stimon):
+	if delayend < i < delayend + (stimoff - stimon):
 		IE = IE - stim
 
 	if i > delayend - delaywin and i <= delayend:
@@ -114,12 +119,12 @@ for i in tqdm(range(1, nb_steps), ascii=True):
 	rE = rE + (f(IE) + noise_E - rE)*dt/tauE
 	rI = rI + (f(II) + noise_I - rI)*dt/tauI
 
-	# Get decoded angle from network activity	plt.plot(theta*180/np.pi, rE)
+	# Get decoded angle from network activity
 
 	# ang = decode(rE, np.transpose(theta))
 	# if i < delayend: response=ang
 	data[i] = rE
 
-#plotting(data, theta)
+pickle.dump(data, open('../Output/data.pkl', 'wb'))
 
 
---
+++ Plot.py
@@ -0,0 +1,22 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+nb_neurons = 512
+
+# Dynamic plotting
+def plotting(data, theta):
+	for step in data:
+		plt.xlim((-180, 180))
+		plt.ylim((-5, 20))
+		plt.title('Iteration {}'.format(step))
+		plt.plot(theta*180/np.pi, data[step])
+		plt.pause(0.001)
+		plt.show()
+		plt.clf()
+
+data = pickle.load(open('../Output/data.pkl', 'rb'))
+theta = np.array([i/nb_neurons*2*np.pi for i in range(0, nb_neurons)]) - np.pi
+
+plotting(data, theta)
