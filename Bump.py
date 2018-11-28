3eb5cca24641226677e527b1d840fa3aa0671172
pouple <antonin@verdier.fr>
2018-11-28 15:53:58 +0100

    Big speed improvments. Changed f-function from vanilla to numpy array. This allows multi-core processing

--- Bump.py
+++ Bump.py
@@ -1,58 +1,55 @@
-# Model bump attractor 
-import numpy as np
 from scipy.linalg import circulant 
 from tqdm import tqdm
-import time
+import numpy as np
 import pickle
-
 import argparse
 
+# Parse arguments
+parser = argparse.ArgumentParser(description='Network Bump Model. It has two rings of neurons interconnected')
+parser.add_argument('--nb_neurons', '-n', type=int, default=512, 
+					help='Number of neurons per ring. Default value is set to 512')
+args = parser.parse_args()
+
+
 def generate_stimulus(stimon, stimoff, stim, delayend):
-	stimulus = np.array(stim * v).reshape(nb_neurons, 1)
-	stimon = np.floor(stimon/dt)
-	stimoff = np.floor(stimoff/dt)
-	delayend = np.floor(delayend/dt)
-	delaywin = np.floor(100/dt)
+	stimulus = np.array(stim * v).reshape(args.nb_neurons, 1)
+	stimon, stimoff = np.floor(stimon/dt), np.floor(stimoff/dt)
+	delayend, delaywin = np.floor(delayend/dt), np.floor(100/dt)
 
 	return stimulus, stimon, stimoff, delayend, delaywin
 
-def f(x):
-	tmp = []
-	for i in x:
-		if 0 < i < 1:
-			tmp.append(i*i)
-		elif i >= 1:
-			tmp.append(np.sqrt(4 * i -3))
-		elif i < 0:
-			tmp.append(0)
-	return np.array(tmp).reshape(nb_neurons, 1)
+def f(a):
+	mask = ((a > 0) & (a < 1))
+	a *= (mask*a + np.invert(mask))
+	
+	a[a < 0] = 0
+	
+	mask = a >= 1
+	a = a - a*mask + np.sqrt(4*(a*mask + 0.75*np.invert(mask)) - 3)
+
+	return a
 
 
 def decode(r, th):
-	return np.arctan2(sum(np.multiply(r, np.sin(th))), sum(np.multiply(r, np.cos(th))))
+	return np.arctan2(np.sum(r*np.sin(th)), np.sum(r*np.cos(th)))
 
 
 def noise(sigE, sigI):
-	noise_E = sigE * np.random.randn(nb_neurons, 1)
-	noise_I = sigI * np.random.randn(nb_neurons, 1)
+	noise_E = sigE * np.random.randn(args.nb_neurons, 1)
+	noise_I = sigI * np.random.randn(args.nb_neurons, 1)
 
 	return noise_E, noise_I
 
-def mat_mul(X, Y):
-	Z = np.zeros([X.shape[0], Y.shape[1]])
-	for i in range(X.shape[0]):
-		for j in range(Y.shape[1]):
-			Z[i, j] = np.sum(X[i:i+1]*np.transpose(Y[:, j:j+1]))
-	return Z
+# def mat_mul(X, Y):
+# 	Z = np.empty([X.shape[0], 1])
+# 	for i in range(X.shape[0]):
+# 		Z[i] = np.sum(X[i]*np.transpose(Y))
+# 	return Z
 
 # Variable definition
-nb_neurons = 512
-nb_population = 8
-
 total_time = 4200
 dt = 2
 nb_steps = int(total_time/dt)
-delayPop = np.zeros([nb_neurons, 1])
 
 # Time constants of neurones equations
 tauE = 20
@@ -68,67 +65,50 @@ GII = 0.85
 I0E = 0.2
 I0I = 0.5
 
-
-stimon = 1000
-stimoff = 1500
-delayend = 1500
-stim = 200
-
 sigE = 1
 sigI = 3
 
-theta = np.array([i/nb_neurons*2*np.pi for i in range(0, nb_neurons)]) # Need to find a more elegant way
+stim = 200
+
+theta = np.array([i/args.nb_neurons*2*np.pi for i in range(0, args.nb_neurons)]) # Need to find a more elegant way
 v = np.exp(kappa*np.cos(theta))
 v = v/sum(v)
 WE = circulant(v)
 
-# Define iterations when stimulus is applied
-
-#### Preliminary calculations #####
-
 # Define matrices initial states
-rE = np.zeros([nb_neurons, 1])
-rI = np.zeros([nb_neurons, 1])
+rE = np.zeros([args.nb_neurons, 1])
+rI = np.zeros([args.nb_neurons, 1])
 
 theta = theta - np.pi
 v = np.exp(kappa*np.cos(theta))
 v = v/sum(v)
 
-stimulus = np.array(stim * v).reshape(nb_neurons, 1)
-stimon = np.floor(stimon/dt)
-stimoff = np.floor(stimoff/dt)
-delayend = np.floor(delayend/dt)
-delaywin = np.floor(100/dt)
+stimulus, stimon, stimoff, delayend, delaywin = generate_stimulus(1000, 1500, 200, 3500)
 
 data = {}
-for i in tqdm(range(1, nb_steps), ascii=True):
-	# Additive noise
-	noise_E, noise_I = noise(sigE, sigI)
-
-	Z = mat_mul(WE, rE)
+for step in tqdm(range(1, nb_steps), ascii=True):
 	
-	IE = GEE*Z + (I0E - GIE*np.mean(rI))*np.ones([nb_neurons, 1])
-	II = (GEI*np.mean(rE) - GII*np.mean(rI) + I0I)*np.ones([nb_neurons, 1])
+	noise_E, noise_I = noise(sigE, sigI)
+	
+	IE = GEE*np.dot(WE, rE) + (I0E - GIE*np.mean(rI))*np.ones([args.nb_neurons, 1])
+	II = (GEI*np.mean(rE) - GII*np.mean(rI) + I0I)*np.ones([args.nb_neurons, 1])
 
-	if stimon < i < stimoff:
+	if stimon < step < stimoff:
 		IE = IE + stimulus
-
-	if delayend < i < delayend + (stimoff - stimon):
+	if delayend < step < delayend + (stimoff - stimon):
 		IE = IE - stim
-
-	if i > delayend - delaywin and i <= delayend:
-		delayPop = delayPop + rE/delaywin
 
 	# Integrate with time step dependant
 	rE = rE + (f(IE) + noise_E - rE)*dt/tauE
 	rI = rI + (f(II) + noise_I - rI)*dt/tauI
 
 	# Get decoded angle from network activity
+	ang = decode(rE, theta.reshape(512, 1))
+	
+	# Save data in object
+	data[step] = [rE, rI, ang] 
 
-	# ang = decode(rE, np.transpose(theta))
-	# if i < delayend: response=ang
-	data[i] = rE
-
+# Save data in dictionnary
 pickle.dump(data, open('../Output/data.pkl', 'wb'))
 
 
--- Plot.py
+++ Plot.py
@@ -13,12 +13,12 @@ def plotting(data, theta):
 		plt.xlim((-180, 180))
 		plt.ylim((-5, 20))
 		plt.title('Iteration {}'.format(step))
-		plt.plot(theta*180/np.pi, data[step])
+		plt.plot(theta*180/np.pi, data[step][0])
 		plt.pause(0.001)
 		plt.show()
 		plt.clf()
 
-data = pickle.load(open('../data2.pkl', 'rb'))
+data = pickle.load(open('../Output/data.pkl', 'rb'))
 theta = np.array([i/nb_neurons*2*np.pi for i in range(0, nb_neurons)]) - np.pi
 
 plotting(data, theta)
